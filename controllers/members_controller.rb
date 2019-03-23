72480ac11cb65c34cf9a210db835f650b1b2cac4
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-23 21:28:13 +0000

    MVP member_controller.rb index, new and show RESTful routes complete index, new and show erb files created and complete links added to index, new and show erb files to take user back to home page

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,7 +3,20 @@ require('sinatra/reloader') if development?
 require_relative('../models/member')
 set :views, Proc.new { File.join(root, "../views") }
 
-get '/members' do
+get '/members' do #index
   @members = Member.all()
   erb(:'members/index')
 end
+
+get '/members/new' do #new
+  erb(:'members/new')
+end
+
+get '/members/:id' do #show
+  @member = Member.find(params[:id])
+  erb(:'members/show')
+end
+
+# post '/members/' do #create
+#
+# end
