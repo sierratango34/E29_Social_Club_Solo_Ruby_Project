3ce14bac6badb9308444f8c242a2e18318b57789
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-23 22:21:33 +0000

    MVP created app.rb complete app.rb requiring relevant controllers created /e29-social-club url within app.rb updated all urls in the controller and /view/members erb files website works as expected after updates to files status -- success

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -3,27 +3,27 @@ require('sinatra/reloader') if development?
 require_relative('../models/member')
 set :views, Proc.new { File.join(root, "../views") }
 
-get '/members' do #index
+get '/e29-social-club/members' do #index
   @members = Member.all()
   erb(:'members/index')
 end
 
-get '/members/new' do #new
+get '/e29-social-club/members/new' do #new
   erb(:'members/new')
 end
 
-get '/members/:id' do #show
+get '/e29-social-club/members/:id' do #show
   @member = Member.find(params[:id])
   erb(:'members/show')
 end
 
-post '/members' do #create
+post '/e29-social-club/members' do #create
   @member = Member.new(params)
   @member.save()
   erb(:'members/create')
 end
 
-get '/members/:id/edit' do
+get '/e29-social-club/members/:id/edit' do
   @member = Member.find(params['id'])
   erb(:'members/edit')
 end
---
+++ views/app.rb
@@ -0,0 +1,9 @@
+require('sinatra')
+require('sinatra/reloader') if development?
+require_relative('controllers/members_controller')
+require_relative('controllers/events_controller')
+require_relative('controllers/bookings_controller')
+
+get '/e29-social-club/' do
+  erb(:index)
+end
