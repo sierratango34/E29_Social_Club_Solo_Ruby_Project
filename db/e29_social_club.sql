36f485a13fdd4531dcafecbb419107d84171baf9
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-23 15:16:22 +0000

    MVP syntax error fixed on e29_social_club.sql

--- db/e29_social_club.sql
+++ db/e29_social_club.sql
@@ -1,21 +1,21 @@
-DROP TABLE IF EXISTS bookings
-DROP TABLE IF EXISTS members
-DROP TABLE IF EXISTS events
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS members;
+DROP TABLE IF EXISTS events;
 
-CREATE TABLE bookings(
-  id SERIAL4 PRIMARY KEY
-  member_id INT4 REFERENCES members(id)
+CREATE TABLE bookings (
+  id SERIAL4 PRIMARY KEY,
+  member_id INT4 REFERENCES members(id),
   event_id INT4 REFERENCES events(id)
 );
 
-CREATE TABLE members(
+CREATE TABLE members (
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
   last_name VARCHAR(255),
   attendance_count INT4
 );
 
-CREATE TABLE events(
+CREATE TABLE events (
   id SERIAL4 PRIMARY KEY,
   type VARCHAR(255),
   number_attending INT4,
