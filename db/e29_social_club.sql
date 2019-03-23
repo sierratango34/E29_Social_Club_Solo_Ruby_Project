95207aa2b58542d9937387f6fc59799b34d89d00
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-23 15:26:55 +0000

    MVP fixed order of tables created in e29_social_club.sql created seed.rb file

--- db/e29_social_club.sql
+++ db/e29_social_club.sql
@@ -2,12 +2,6 @@ DROP TABLE IF EXISTS bookings;
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS events;
 
-CREATE TABLE bookings (
-  id SERIAL4 PRIMARY KEY,
-  member_id INT4 REFERENCES members(id),
-  event_id INT4 REFERENCES events(id)
-);
-
 CREATE TABLE members (
   id SERIAL4 PRIMARY KEY,
   first_name VARCHAR(255),
@@ -20,4 +14,10 @@ CREATE TABLE events (
   type VARCHAR(255),
   number_attending INT4,
   max_capacity INT4
+);
+
+CREATE TABLE bookings (
+  id SERIAL4 PRIMARY KEY,
+  member_id INT4 REFERENCES members(id),
+  event_id INT4 REFERENCES events(id)
 );
