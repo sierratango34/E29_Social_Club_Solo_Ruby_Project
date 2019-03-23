cefa62431937e3799fcac1501722922548651580
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-23 18:16:40 +0000

    added gym brief for reference

--- controllers/members_controller.rb
+++ controllers/members_controller.rb
@@ -1,7 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('../models/members')
-also_reload('../models/members')
+require_relative('../models/member')
+also_reload('../models/member')
 
 get '/members' do
   @members = Member.all
