ed41c410fc44c46dd8a93571752d0c674d413f17
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-24 10:59:47 +0000

    MVP new route in RESTful routes created and complete in booking_controller.rb new.erb created and complete with basic form requiring text input, but ideally will be changed to drop down selection of appropriate members and events submit button added to new.erb link to add new booking added to index.erb website working as expected

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 require('sinatra')
 require('sinatra/reloader') if development?
 require_relative('../models/booking')
+require_relative('../models/member')
+require_relative('../models/event')
 set :views, Proc.new { File.join(root, "../views") }
 
 get '/e29-social-club/bookings' do
@@ -8,11 +10,10 @@ get '/e29-social-club/bookings' do
   erb(:'bookings/index')
 end
 
-# get 'e29-social-club/bookings/new' do
-#   @booking = Booking.new(params)
-#   erb(:'bookings/new')
-# end
-
+get '/e29-social-club/bookings/new' do
+  @booking = Booking.new(params)
+  erb(:'bookings/new')
+end
 
 get '/e29-social-club/bookings/:id' do
   @booking = Booking.find(params[:id])
