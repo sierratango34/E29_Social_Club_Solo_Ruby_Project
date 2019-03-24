e299e27bab51be9fe986bd79e2345a1bb60da274
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-24 19:42:09 +0000

    MVP app.rb require_relative paths fixed created links to members, events and bookings on app.rb (main landing page) website works as expected status -- success Version 1.0.0 No CSS or styling

--- views/app.rb
+++ views/app.rb
@@ -1,8 +1,8 @@
 require('sinatra')
 require( 'sinatra/contrib/all' )
-require_relative('controllers/members_controller')
-require_relative('controllers/events_controller')
-require_relative('controllers/bookings_controller')
+require_relative('../controllers/members_controller')
+require_relative('../controllers/events_controller')
+require_relative('../controllers/bookings_controller')
 
 get '/e29-social-club' do
   erb(:index)
