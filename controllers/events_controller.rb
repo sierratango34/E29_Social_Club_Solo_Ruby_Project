400cce0d1fce62563e1fcd0ce33c88c3d8374cea
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-03-24 08:22:03 +0000

    MVP new route in RESTful routes complete new erb created and complete with appropriate link back to events home page

--- controllers/events_controller.rb
+++ controllers/events_controller.rb
@@ -7,3 +7,12 @@ get '/e29-social-club/events' do
   @events = Event.all()
   erb(:'events/index')
 end
+
+get '/e29-social-club/events/new' do
+  erb(:'events/new')
+end
+
+# get '/e29-social-club/events/:id' do
+#   @event = Event.find(params[:id])
+#   erb(:'events/show')
+# end
