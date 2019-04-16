d86749147f8acc2747009cef31992519cbf61f9c
Kanokwan Sritawan <k.sritawan@gmail.com>
2019-04-16 21:11:33 +0100

    ready for Heroku

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,10 +1,21 @@
 require( 'pg' )
 
 class SqlRunner
+  # original link for localhost
+  # def self.run( sql, values = [] )
+  #   begin
+  #     db = PG.connect({ dbname: 'e29_social_club', host: 'localhost' })
+  #     db.prepare("query", sql)
+  #     result = db.exec_prepared( "query", values )
+  #   ensure
+  #     db.close() if db != nil
+  #   end
+  #   return result
+  # end
 
   def self.run( sql, values = [] )
     begin
-      db = PG.connect({ dbname: 'e29_social_club', host: 'localhost' })
+      db = PG.connect({ dbname: 'd459ec5hh2l4u6', host: 'ec2-54-246-92-116.eu-west-1.compute.amazonaws.com', port: 5432, user: 'btwpibjjsctrfx', password: 'sadlskjadlkjASDAD' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
