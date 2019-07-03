ac4cee0aa3ddc7c9191386ae3e92e677a185eca2
sierratango34 <k.sritawan@gmail.com>
2019-07-04 00:02:35 +0100

    cchanges to run locally

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -3,12 +3,7 @@ require( 'pg' )
 class SqlRunner
   def self.run( sql, values = [] )
     begin
-      # db = PG.connect({ dbname: 'e29_social_club', host: 'localhost' })
-      db = PG.connect({dbname: ENV['DBNAME'],
-        host: ENV['DBHOST'],
-        port: ENV['DBPORT'],
-        user: ENV['DBUSER'],
-        password: ENV['DBPASSWORD']})
+      db = PG.connect({ dbname: 'e29_social_club', host: 'localhost' })
       db.prepare("query", sql)
       result = db.exec_prepared( "query", values )
     ensure
